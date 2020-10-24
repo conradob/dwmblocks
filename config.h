bbfe0f9382ee402efcf7082c140c7e7b4593e1d3
Conrado Fonseca <conradob@live.com>
2020-10-24 20:56:52 +0100

    My config

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* {"⌨", "kbselect", 0, 30}, */
+	{"",	"keyboardlayout",	5,	3},
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"music",	0,	11}, */
 	{"",	"pacpackages",	0,	8},
@@ -14,21 +15,21 @@ static const Block blocks[] = {
 	/* {"",	"price eth Ethereum 🍸",	9000,	23}, */
 	/* {"",	"price btc Bitcoin 💰",				9000,	21}, */
 	/* {"",	"torrent",	20,	7}, */
-	/* {"",	"memory",	10,	14}, */
-	/* {"",	"cpu",		10,	18}, */
+	{"",	"memory",	10,	14},
+	{"",	"cpu",		10,	18},
 	/* {"",	"moonphase",	18000,	17}, */
-	{"",	"forecast",	18000,	5},
 	{"",	"mailbox",	180,	12},
 	{"",	"nettraf",	1,	16},
-	{"",	"volume",	0,	10},
-	{"",	"battery",	5,	3},
 	{"",	"clock",	60,	1},
 	{"",	"internet",	5,	4},
-	{"",	"help-icon",	0,	15},
+	{"",	"volume ",	0,	10},
+	{"",	"forecast",	18000,	5},
+	{"",	"battery",	5,	3},
+	// {"",	"help-icon",	0,	15},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = " │ ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
