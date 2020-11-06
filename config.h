c5b58bcb4c6a53dc79904e1c4b8ef21743ac9155
Conrado Fonseca <conradob@live.com>
2020-11-06 12:30:13 +0000

    Moving

--- config.h
+++ config.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"⌨", "kbselect", 0, 30}, */
+	// {"⌨", "kbselect", 0, 30},
+	{"",	"music",	10,	11},
 	{"",	"keyboardlayout",	5,	3},
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/* {"",	"music",	0,	11}, */
+	// {"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	{"",	"pacpackages",	0,	8},
 	{"",	"news",		0,	6},
 	/* {"",	"crypto",	0,	13}, */
@@ -20,11 +20,11 @@ static const Block blocks[] = {
 	/* {"",	"moonphase",	18000,	17}, */
 	{"",	"mailbox",	180,	12},
 	{"",	"nettraf",	1,	16},
-	{"",	"clock",	60,	1},
 	{"",	"internet",	5,	4},
 	{"",	"volume ",	0,	10},
 	{"",	"forecast",	18000,	5},
 	{"",	"battery",	5,	3},
+	{"",	"clock",	60,	1},
 	// {"",	"help-icon",	0,	15},
 };
 
